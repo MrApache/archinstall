2c9862c2a28f509a40f09a53d53e6923d0856cfc
Irisu <mrapacheee@gmail.com>
2024-05-18 14:55:57 +0400

    Added bash script

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+echo "Enter hostname:"
+read HOSTNAME
+
+echo "Enter username:"
+read USERNAME
+
+echo "Enter user password:"
+read USERPASS
+
+echo "Enter root password:"
+read ROOTPASS
+
+echo "Enter /dev/DISK:"
+read DISK
+
+TIME=Europe/Samara
+EFI="${DISK}1"
+SWAP="${DISK}2"
+ROOT="${DISK}3"
+
+timedatectl set-timezone TIME
+
+parted $DISK mklabel gpt
+parted set 1 esp on
+parted $DISK mkpart "EFI system partition" fat32 1MiB 261MiB
+parted $DISK mkpart "swap" linux-swap 261MiB 4G
+parted $DISK mkpart "linux" btrfs 4G 100%
+
+mkfs.fat -F 32 $EFI
+mkswap $SWAP
+mkfs.btrfs $ROOT
+
+mount $ROOT /mnt
+mount --mkdir $EFI /mnt/boot
+swapon $SWAP
+
+pacstrap -K /mnt base linux linux-firmware neovim
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
+ln -sf /usr/share/zoneinfo/$TIME /etc/localtime
+hwclock --systohc
+
+#(нужно отредактировать /etc/locale.gen)
+#(удалить символ # у ru и en локалей)
+sed -i -e 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
+sed -i -e 's/#ru_RU.UTF-8/ru_RU.UTF-8/g' /etc/locale.gen
+locale-gen
+printf 'LANG=ru_RU.UTF-8' > /etc/locale.conf
+printf $HOSTNAME > /etc/hostname
+
+passwd $ROOTPASS
+useradd -m $USERNAME
+echo $USERPASS | passwd $USERNAME
